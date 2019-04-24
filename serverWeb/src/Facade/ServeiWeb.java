4e1d01a9a961eaa07b11e57a06773b5a0718ef11
adricasi <adricasi@users.noreply.github.com>
2019-04-24 18:35:35 +0200

    Added CercaTipoLocal function

--- serverWeb/src/Facade/ServeiWeb.java
+++ serverWeb/src/Facade/ServeiWeb.java
@@ -1,9 +1,12 @@
 package Facade;
 import bean.Local;
+import bean.TipoLocal;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
@@ -15,7 +18,7 @@ import bean.Accessibilitat;
 @WebService
 public class ServeiWeb {
 
-	@WebMethod
+	/*@WebMethod
 	public void AltaLocal(Local local, List<Accessibilitat> accessibilitat) {
 				
 		System.out.print("\n AltaLocal");
@@ -50,7 +53,7 @@ public class ServeiWeb {
 				e.printStackTrace();
 			}
 		}		
-	}
+	}*/
 	
 	
 	
@@ -63,6 +66,86 @@ public class ServeiWeb {
 	@WebMethod
 	public void BaixaLocal(int codiLocal) {
 		
+	}
+	
+	@WebMethod
+	public Local InfoLocal(int codiLocal) {
+		String strEstat = new String();
+		Connection connection = null;
+		
+		Local local = new Local();
+		
+		try{	
+			InitialContext cxt = new InitialContext();
+			if ( cxt != null ){
+				DataSource ds = (DataSource) cxt.lookup( "java:jboss/PostgreSQL/eAccessible");
+				if ( ds == null ) strEstat = "Error al crear el datasource";
+				else{
+					connection = ds.getConnection();
+					
+					String query = "select * from eAccessible.local where codilocal="+codiLocal;
+					Statement stm = connection.createStatement();
+					ResultSet rs = stm.executeQuery(query);
+					
+					local.setCoditipolocal(rs.getInt("coditipolocal"));
+					local.setCodicarrer(rs.getInt("codicarrer"));
+					local.setNomcarrer(rs.getString("nomcarrer"));
+					local.setNomvia(rs.getString("nomvia"));
+					local.setCodilocal(codiLocal);
+					local.setNomlocal(rs.getString("nomlocal"));
+		
+					
+					connection.close();
+					stm.close();
+				}
+			}
+		}
+		
+	}
+	
+	@WebMethod
+	public List<TipoLocal> CercaTipoLocal() {
+				
+		String strEstat = new String();
+		Connection connection = null;
+		
+		List<TipoLocal> tipoLocalList = new ArrayList<TipoLocal>();
+		
+		try{	
+			InitialContext cxt = new InitialContext();
+			if ( cxt != null ){
+				DataSource ds = (DataSource) cxt.lookup( "java:jboss/PostgreSQL/eAccessible");
+				if ( ds == null ) strEstat = "Error al crear el datasource";
+				else{
+					connection = ds.getConnection();
+					
+					String query = "select coditipolocal, nomtipolocalca, nomtipolocales, nomtipolocalen from eAccessible.tipolocal";
+					Statement stm = connection.createStatement();
+					ResultSet rs = stm.executeQuery(query);
+					while(rs.next()) {
+						TipoLocal tipoLocal = new TipoLocal();
+						tipoLocal.setCoditipolocal(rs.getInt("coditipolocal"));
+						tipoLocal.setNomtipolocalca(rs.getString("nomtipolocalca"));
+						tipoLocal.setNomtipolocales(rs.getString("nomtipolocales"));
+						tipoLocal.setNomtipolocalen(rs.getString("nomtipolocalen"));
+						
+						tipoLocalList.add(tipoLocal);
+					}
+					connection.close();
+					stm.close();
+				}
+			}
+		}
+		catch(Exception e) {e.printStackTrace();}
+		finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return tipoLocalList;
 	}
 	
 }
